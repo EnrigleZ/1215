8f3d597cd40ef551b241fb93f07af8e7d2aa78a7
Yucong Zhou <zhouyucong0421@foxmail.com>
2020-12-27 00:13:42 +0800

    feat: add hour delta mechanism
    - feat: prevent data loss when reaching upper-limit (60000)
    - feat: add delta_hours and save arguments
    - feat: auto decrease delta when reaching upper-limit
    - todo: find a better solution for auto-decreasing

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## 调用方式
 
 ```bash
-python ./main --start <开始日期+时间> --end <结束日期+时间> --equipments <查询设备>
+python ./main --start <开始日期+时间> --end <结束日期+时间> --equipment <查询设备> --delta_hours <单次查询小时> --save <保存文件名>
 ```
 
 ## 参数说明
@@ -15,10 +15,12 @@ python ./main --start <开始日期+时间> --end <结束日期+时间> --equipm
   - 仅 `end` 为空，则设置为查询从 `start` 到今日结束 `"235959"` 的结果。
   - 仅 `start` 为空，会报错。
 
-- `--equipments`: 查询设备， 支持 `"ABGH"`，例如 `"AB"` 代表仅查询AB两台设备。
+- `--equipment`: 查询设备， 支持 `"ABGH"`，例如 `"A"` 代表仅查询A设备。
+- `--delta_hours`: 每次间隔时间，为了防止单次查询达到上限`60000`条导致的数据丢失，默认为`4`，代表每次查询四个小时的记录。
+- `--save`: 保存文件名，默认为时间+设备。
 
 ## 调用示例
 
 ```basg
-python ./main.py --start 20201219
+python ./main.py --start 20201219 --end 20201219-235959 --equipment A --delta_hours 4
 ```
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 '''这个文档是程序入口，框架函数
 '''
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.parser import parse as dateParser
 
 from processor import Processor
 import utils
 
-def main_loop(proc: Processor, start_time: datetime = None, end_time: datetime = None, equipments: str = "ABGH"):
+RESULT_UPPER_LIMIT = 60000
+
+def main_loop(proc: Processor,
+              start_time: datetime = None,
+              end_time: datetime = None,
+              equipment: str = "ABGH",
+              delta: timedelta = None,
+              save_filename: str = None
+              ):
     '''主循环，每次查找 10000 条记录后，自动换下一批
     '''
-
     # 在 Request 参数中设定时间，设备这些参数
     if end_time is None:
         if start_time is None:
@@ -19,43 +26,65 @@ def main_loop(proc: Processor, start_time: datetime = None, end_time: datetime =
     else:
         assert start_time, "Please enter a '--start' argument, "\
             "e.g., 'python ./main.py --start 20201215-003000'"
-    equip_param = utils.get_equip_param(equipments)
+
+    if save_filename is None:
+        pattern = "%Y%m%d-%H%M%S"
+        str_start = utils.datetime2Str(start_time, pattern)
+        str_end = utils.datetime2Str(end_time, pattern)
+        save_filename = f"{str_start}__{str_end}__{equipment}"
+        save_filename += ".xlsx"
+
+    print("---------------------------")
+    print("Start Time:", start_time)
+    print("End Time:", end_time)
+    print("Equipment:", equipment)
+    print("Save File:", save_filename)
+    print("---------------------------")
+
+    assert len(equipment) == 1, f"Only fetch one equipment once. (Received: {equipment})"
+    equip_param = utils.get_equip_param(equipment)
 
     proc.updateField("StartTime", utils.datetime2Str(start_time))
     proc.updateField("EndTime", utils.datetime2Str(end_time))
     proc.updateField("EquipmentId", equip_param)
 
+
+    end_search_time = end_time
+    start_search_time = max(start_time, end_time - delta)
+
     results = []
     while True:
-        # 查询一批，如果超过 10000 条上限，系统会返回时间最近的一批记录
+        # 查询一次，查询时间长度为`delta_hours`
+        proc.updateField("StartTime", utils.datetime2Str(start_search_time))
+        proc.updateField("EndTime", utils.datetime2Str(end_search_time))
         print(proc.getField("StartTime"), "---", proc.getField("EndTime"))
+
         dom = proc.fetch()
         rows = proc.parseRows(dom)
+        print(f"* Fetch {len(rows)} new records.")
+
+        # 如果达到查询上限，为了防止数据丢失，丢弃本次查询结果并降低delta重新查询
+        if len(rows) == RESULT_UPPER_LIMIT:
+            print(f"返回结果达到上限 {RESULT_UPPER_LIMIT} 条，自动调低 delta 重试")
+            delta = timedelta(seconds=delta.seconds / 2)
+            start_search_time = end_search_time - delta
+            continue
 
         # 从新获取到的内容里去重，线性时间复杂度，然后添加到全部记录结果`results`里
-        # 如果全部重复，则证明查询结束
         cut_index = utils.compare_rows(results, rows)
-        if cut_index == len(rows):
-            print(f"* Done. Fetched {len(results)} records")
-            break
-
         print(f"* Add {len(rows) - cut_index} new records.")
         results.extend(rows[cut_index: ])
 
-        # 把本次获取到的结果里最早的时间，设置成下一次查询的结束时间
-        # 会有一部分的重叠，所以需要先前的去重步骤
-        last_datetime = proc.parseLastDateTime(dom)
-        new_endtime_str = utils.datetime2Str(last_datetime)
-        proc.updateField("EndTime", new_endtime_str)
+        # Break 条件
+        if end_search_time == start_search_time:
+            print(f"* Done. Fetched {len(results)} records")
+            break
 
-    # 保存 Excel 文件到`./results`目录下，文件名为时间区间，精确到秒
-    pattern = "%Y%m%d-%H%M%S"
-    str_start = utils.datetime2Str(start_time, pattern)
-    str_end = utils.datetime2Str(end_time, pattern)
+        # 更新查询时间到下一时间段
+        end_search_time = start_search_time
+        start_search_time = max(end_search_time - delta, start_time)
 
-    save_filename = f"{str_start}__{str_end}"
-    save_filename += ".xlsx"
-
+    # 保存 Excel 文件到`./results`目录下，默认文件名为时间区间，精确到秒
     utils.save_xlsx(
         header=proc.parseHeader(proc.last_dom),
         rows=results,
@@ -68,16 +97,21 @@ def __main__():
 
     parser.add_argument("--start", type=str, required=False, help='e.g., "20201201-103000"')
     parser.add_argument("--end", type=str, required=False, help='e.g., "20201201-235959"')
-    parser.add_argument("--equipments", type=str, default="ABGH", help='e.g., "ABGH"')
+    parser.add_argument("--equipments", type=str, default="A", help='e.g., "ABGH"')
+    parser.add_argument("--delta_hours", type=int, default=4, help='e.g., 4')
+    parser.add_argument("--save", type=str, default='', help='Save filename')
 
 
     args = parser.parse_args()
     start = dateParser(args.start) if args.start else None
     end = dateParser(args.end) if args.end else None
+    equipments = args.equipments
+    delta_hours = timedelta(hours=args.delta_hours)
+    save_name = args.save or None
 
     # 开始循环
     proc = Processor()
-    main_loop(proc, start, end, args.equipments)
+    main_loop(proc, start, end, equipments, delta_hours, save_name)
 
 if __name__ == "__main__":
     __main__()
